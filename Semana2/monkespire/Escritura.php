9083ffc69782145e87cba38e43a161c8c4811dd0
RUVS <321064844@alumno.enp.unam.mx>
2023-06-05 23:39:01 -0600

    quité lineas comentadas

--- Semana2/monkespire/Escritura.php
+++ Semana2/monkespire/Escritura.php
@@ -1,27 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Monkespirre</title>
     <link rel="icon" href="https://topmusic.mx/wp-content/uploads/2018/07/upscale-235527132031212.png" type="image/x-png">
-
 </head>
 
 <body>
-
 <Form action="Escritura.php" method="post">
         <fieldset>
             <legend><h1>Escritura de monos</h1></legend>
-
-            <label for="frase"> Texto: </label>
-           
+            <label for="frase"> Texto: </label>        
                <textarea name="frase" id="frase" cols="50" rows="1" maxlength="200" placeholder="Escribe una frase"></textarea>
-
             <br> <br>
-            Tipo de frase:
-                       
+            Tipo de frase                    
             <select name="tipo" id="tipo">
                 <option value=1>Palabras juntas y ordenadas</option>
                 <option value=2>Palabras sueltas pero ordenadas</option>
@@ -34,18 +28,13 @@
                 <option>Palabras</option>
                 <option>Palabras desordenadas </option>
             </select>
-            <br><br>
-            
-            <br>
-
-    <center><img src="https://www.elsiglodetorreon.com.mx/m/i/2016/09/863054.jpeg" width="300"></center>
-  
+            <br><br><br>
+    <center><img src="https://www.elsiglodetorreon.com.mx/m/i/2016/09/863054.jpeg" width="300"></center> 
             <button type="submit">Enviar</button>
             
         </fieldset> 
     </Form>
 <?php   
-
 $texto1 = (isset($_POST['frase']) && $_POST["frase"] != "")? $_POST['frase'] : "";// guradar $textointroducido ---------------------------
 if ($texto1!=""){
 $palabras = array("");
@@ -55,30 +44,19 @@ if ($_POST["tipo"] == "3")
         // $arreglo1= la localidad cero guarda " "---------------------------
         //si tipo 1 $arreglo1= texto grande en localidad 1
         //si no separar  $textointroducido por palabras (con la funcion explode quiatndo espacios) y guandar en $arreglo1 cada palabra en una localidad de numero entero consecutivo (la localidad 0 se mantiene igual)
-
-
 $lda = 0 ;
-
 foreach($palabras as $valor){
 $lda ++;
 }
-
-
-
-$lep=0;
+$lep=0; //letras en palabra
 $cdpi=0;//cantidad de palabras intermedias
-
-
-
 $letras = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890$%&';
-
  $lep =0;
  $cdpi=0;//cantidad de palabras intermedias
  $pdal=0;//palabras de arreglo leidas
 
  while ($pdal!=$lda)
  {
-
     if ($cdpi==0 )
     {
         echo "<u>","<i>","<strong>" , $palabras[$pdal],"</i>","</u>" , "</strong>"," ";//en negritas
@@ -86,8 +64,6 @@ $letras = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890$%&';
         $pdal++;
         $cdpi=rand( round((350-$lda)/$lda*0.6),round((350-$lda)/$lda*1.4));
     }
-
-
     if ($lep>0)
     {
         print($letras[rand(0, strlen($letras) - 1)]);
@@ -100,97 +76,8 @@ $letras = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890$%&';
         $lep=rand(0,20);
         $cdpi--;
     }
-
-
 }
-
-}
-/*   
-foreach($palabras as $palabra)
-{
-   if ($cdpi<=0)
-   {
-    echo "hola" ;
-       $cdpi=rand(0,$ndem);
-   }
-   
-
-   if ($lep>=0)
-   {
-       print("-");
-       $lep=rand(0,20);
-   }
-   if ($lep<0)
-   {
-       print("a");
-       $lep=rand(0,20);//letras en palabra 
-       $cdpi--;
-   }
-}
-  
-    
-    
-
- //-----------guia y esqueleto monkespeare  --------------------------------------- 
-  
-
-        
-
-      // Generar letras y caracteres aleatorios
- //
-
-
-
-
-
- while {$pdal<=$lda}
- {
-    if ($cdp<=0)
-    {
-        print($arreglo1[$pdal]);//en negritas
-        $pdal++;
-        $cdpi=rand(0,$ndem);
-    }
-    
-
-    if ($lep>=0)
-    {
-        print(//letra aleatoria);
-        $lep=rand(0,20);
-    }
-    if ($lep<0)
-    {
-        print(" ");
-        $lep=rand(0,20);
-        $cdpi--;
-    }
-}
-        //Resultado 300 palabras de 1 a 20 caracteres aleatorios 
- //-----------fin de guia y esqueleto monkespeare  --------------------------------------- 
-
-
-        function LetAl($longitud) {
-          $letras = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890$%&';
-          $resultado = '';
-          $len_cadena = strlen($letras) - 1;
-          
-          for ($i = 0; $i < $longitud; $i++) {
-            $num_aleat = mt_rand(0, $len_cadena);
-            $resultado .= $letras[$num_aleat];
-          }
-          return $resultado;
-        }
-        
-        // Ejemplo de uso
-        $longitudDeseada = 400;
-        $texto_aleatorio = LetAl($longitudDeseada);
-        echo "Texto aleatorio: " . "<p>". $texto_aleatorio. "</p>". "<br><br>";
-        var_dump($letras);
-        
-*/
-        
-    ?>
-   
-   
+} 
+ ?>      
 </body>
 </html>
